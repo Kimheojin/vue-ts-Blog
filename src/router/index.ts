c18327926f078368838dbf20cd4f215a880d21b8
jin <hurjin1109@naver.com>
2025-03-24 17:36:52 +0900

    init

---
+++ src/router/index.ts
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from "../App.vue";
+import HelloWorld from "../components/HelloWorld.vue";
+
+
+const routes = [
+    {
+        path: '/',
+        component: App
+    },
+    {
+        path: '/test',
+        component: HelloWorld
+    }
+
+];
+
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
