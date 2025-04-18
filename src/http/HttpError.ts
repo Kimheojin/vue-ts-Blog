26d0a8a2f70b4639b9338a887879afc90308b9f2
jin <hurjin1109@naver.com>
2025-04-18 18:05:11 +0900

    FIX : tsyringe(의존성 주입 관련) 라이브러리 import 위치 수정

--- src/http/HttpError.ts
+++ src/http/HttpError.ts
@@ -7,7 +7,7 @@ export default class HttpError{
     private readonly code: string
     private readonly message: string
 
-    constructor(e: AxiosError<ErrorResponse>) {
+    constructor(e: AxiosError<ErrorResponse>) { // 디폴트 오류
         this.code = e.response?.data?.code || '500'
         this.message = e.response?.data?.message || '네트워크 상태 확인 필요'
     }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
+import 'reflect-metadata';
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router';
 import 'element-plus/theme-chalk/dark/css-vars.css'
-import 'reflect-metadata';
+
 
 const app = createApp(App)
 app.use(router);
