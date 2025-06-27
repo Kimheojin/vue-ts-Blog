24532bfe9cfadc8ae4e008e02ad2d23bc1985885
jin <hurjin1109@naver.com>
2025-06-27 19:37:45 +0900

    FEAT : 이미지 삭제, ㄹ스트 반환 메소드 추가

--- src/repository/image/ImageRepository.ts
+++ src/repository/image/ImageRepository.ts
@@ -2,6 +2,11 @@ import {inject, singleton} from "tsyringe";
 import HttpRepository from "../HttpRepository.ts";
 import type ImageUploadRequest from "../../entity/image/request/ImageUploadRequest.ts";
 import ImageUploadResponse from "../../entity/image/response/ImageUploadResponse.ts";
+import type ImageListRequest from "../../entity/image/request/ImageListRequest.ts";
+import type ImageItem from "../../entity/image/data/ImageItem.ts";
+import ImageListResponse from "../../entity/image/response/ImageListResponse.ts";
+import type ImageDeleteRequest from "../../entity/image/request/ImageDeleteRequest.ts";
+import ImageDeleteResponse from "../../entity/image/response/ImageDeleteResponse.ts";
 
 
 @singleton()
@@ -12,7 +17,9 @@ export default class ImageRepository {
 
     ) {}
 
-    public async upladImage(request: ImageUploadRequest): Promise<ImageUploadResponse>{
+    
+    // 이미지 업로드
+    public async uploadImage(request: ImageUploadRequest): Promise<ImageUploadResponse>{
         const formData = new FormData();
 
         if(request.file){ // 존재할 경우
@@ -28,4 +35,28 @@ export default class ImageRepository {
             }, ImageUploadResponse);
         return response
     }
+
+    
+    // 이미지 리스트 반환
+    public async getImageList(request: ImageListRequest): Promise<ImageItem[]> {
+        const response = await this.httpRepository.get<ImageListResponse>({
+            path: '/api/admin/images',
+            params: { folder: request.folder}
+        }, ImageListResponse)
+
+        return response.images || [];
+    }
+
+
+    // 이미지 삭제
+    public async deleteImage(request: ImageDeleteRequest): Promise<ImageDeleteResponse> {
+        const response = await this.httpRepository.delete<ImageDeleteResponse>({
+            path: '/api/admin/images',
+            body: request
+        }, ImageDeleteResponse);
+
+        return response;
+    }
+
+
 }
