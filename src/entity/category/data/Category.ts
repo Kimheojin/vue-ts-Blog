688e4b1502dcda21cb0897d1a55b7e3cda6e3339
jin <hurjin1109@naver.com>
2025-07-04 21:56:25 +0900

    FEAT : catgory 관련 priority필드 추가

--- src/entity/category/data/Category.ts
+++ src/entity/category/data/Category.ts
@@ -1,4 +1,5 @@
 export default class Category{
     public categoryId: number = 0
     public categoryName: string = ""
+    public priority: number = 0
 }
--- src/entity/category/request/CategoryModifyRequest.ts
+++ src/entity/category/request/CategoryModifyRequest.ts
@@ -1,4 +1,5 @@
 export default class CategoryModifyRequest{
     public categoryId: number = 0
     public categoryName: string = ""
+    public priority: number = 0;
 }
